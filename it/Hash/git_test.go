b58406db8bea3071c3fc221304e7cfe3bb6c67b9
namekridchai <66484292+namekridchai@users.noreply.github.com>
2025-02-15 23:40:39 +0700

    test:get correct content with hash file function

--- it/Hash/git_test.go
+++ it/Hash/git_test.go
@@ -13,27 +13,51 @@ func TestHashShouldGetCorrectFileName(t *testing.T) {
 	GitRootdir := ".cgit"
 	os.Mkdir(GitRootdir, 0755)
 	path := "git_test.go"
-	content, _ := os.ReadFile(path)
-	expect_content := "blob" + "\x00" + string(content)
+	expect, _ := os.ReadFile(path)
+	expect_content := "blob" + "\x00" + string(expect)
 
 	hash := sha256.New()
 	hash.Write([]byte(expect_content))
 	hashBytes := hash.Sum(nil)
 	hashString := hex.EncodeToString(hashBytes)
-	targetPath := GitRootdir + "/object/" + hashString
-	// actual, _ := os.ReadFile(targetPath)
+	hashedFilePath := GitRootdir + "/object/" + hashString
+	// actual, _ := os.ReadFile(hashedFilePath)
 
 	command.Hash(path, "blob")
 
-	dir, err := os.Stat(targetPath)
+	dir, err := os.Stat(hashedFilePath)
 	if err != nil {
 		if os.IsNotExist(err) {
-			t.Fatalf("%v does not exist", targetPath)
+			t.Fatalf("%v does not exist", hashedFilePath)
 		}
 		t.Fatalf("Hash should create directory successfully but got error %v", err)
 	}
 	if dir.IsDir() {
 		t.Fatal("hash does not create file but directory")
+	}
+
+	os.RemoveAll(GitRootdir)
+
+}
+
+func TestHashShouldGetCorrectContent(t *testing.T) {
+	GitRootdir := ".cgit"
+	os.Mkdir(GitRootdir, 0755)
+	path := "git_test.go"
+	expect, _ := os.ReadFile(path)
+	expect_content := "blob" + "\x00" + string(expect)
+
+	hash := sha256.New()
+	hash.Write([]byte(expect_content))
+	hashBytes := hash.Sum(nil)
+	hashString := hex.EncodeToString(hashBytes)
+	hashedFilePath := GitRootdir + "/object/" + hashString
+
+	command.Hash(path, "blob")
+	actual, _ := os.ReadFile(hashedFilePath)
+
+	if string(expect_content) != string(actual) {
+		t.Fatalf("expect %v but got %v", string(expect_content), string(actual))
 	}
 
 	os.RemoveAll(GitRootdir)
