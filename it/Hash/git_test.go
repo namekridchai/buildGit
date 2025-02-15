8d996c09593c2609b69ec23aafd8cf731c1fe8e0
namekridchai <66484292+namekridchai@users.noreply.github.com>
2025-02-15 23:30:12 +0700

    test : hash get correct file name

---
+++ it/Hash/git_test.go
@@ -0,0 +1,40 @@
+package mainTest
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"os"
+	"testing"
+
+	"github.com/namekridchai/buildGit/command"
+)
+
+func TestHashShouldGetCorrectFileName(t *testing.T) {
+	GitRootdir := ".cgit"
+	path := "git_test.go"
+	content, _ := os.ReadFile(path)
+	expect_content := "blob" + "\x00" + string(content)
+
+	hash := sha256.New()
+	hash.Write([]byte(expect_content))
+	hashBytes := hash.Sum(nil)
+	hashString := hex.EncodeToString(hashBytes)
+	targetPath := GitRootdir + "/object/" + hashString
+	// actual, _ := os.ReadFile(targetPath)
+
+	command.Hash(path, "blob")
+
+	dir, err := os.Stat(targetPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			t.Fatalf("%v does not exist", targetPath)
+		}
+		t.Fatalf("Hash should create directory successfully but got error %v", err)
+	}
+	if dir.IsDir() {
+		t.Fatal("hash does not create file but directory")
+	}
+
+	os.RemoveAll(GitRootdir)
+
+}
